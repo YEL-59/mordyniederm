8f466dd492dc8ed382b7a5d6cbceac6084cd3946
tofayel <tofayeltuhin143@gmail.com>
2025-04-22 15:37:26 +0600

    add why chose us pages

--- src/pages/main/home/index.jsx
+++ src/pages/main/home/index.jsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/ui/main/home/hero";
+import { Whychoseus } from "@/components/ui/main/home/whychoseus";
 
 const Home = () => {
   return (
     <>
       <div>
         <Hero />
+        <Whychoseus />
       </div>
     </>
   );
